1afb51f4ce02d9345f1a699a92943ffbf58ae241
ChristosCM <christoumavropoulos.christos@gmail.com>
2019-01-30 11:39:32 +0000

    latest build of the site, minor features need to be implemented

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
- var Storage = multer.diskStorage({
+var Storage = multer.diskStorage({
 
     destination: function(req, file, callback) {
 
@@ -18,13 +18,13 @@ app.use(bodyParser.json());
     },
 
     filename: function(req, file, callback) {
-
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+            callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    
     }
 
 });
 var upload = multer({
-
+    
     storage: Storage
 
 }).array("imgUploader", 3); //Field name and max count
@@ -32,7 +32,6 @@ var upload = multer({
 
 app.get('/people', function (req, res) {
     fs.readFile( __dirname + "/" + "people.json", 'utf8', function (err, data) {
-    //    console.log( data );
        res.type('json');
        res.end( data );
     });
@@ -50,16 +49,49 @@ app.get('/people/:username', function (req,res) {
 
     });
 })
+app.get('/images', function(req,res){
+    var categories = req.query.cat
+
+    var sendIm = [];
+    fs.readFile("./Images/images.json" ,function(err,data){
+        var images = JSON.parse(data);
+        for (i=0; i<images.length; i++){
+            for (y=0; y<categories.length; y++){
+                if (images[i].category == categories[y]){
+                    sendIm.push(images[i]);
+                }
+            }
+        }
+        
+        res.send(JSON.stringify(sendIm))
+        
+    });
+    
+    //res.send(JSON.stringify(sendIm));
+     
+});
+function exists(user){
+    fs.readFile( __dirname + "/" + "people.json", 'utf8', function (err, data) {
+        var people = JSON.parse(data)
+        ex = false;
+        for (var i=0; i<people.length; i++){
+            if (people[i].username == user){
+                ex = true;
+            }
+        }
+    return ex;
+    });
+}
 
  app.post('/people', function (req, res) {
     fs.readFile( __dirname + "/" + "people.json", 'utf8', function (err, data) {
         var people = JSON.parse(data)
-        if (req.get('access_token') != "concertina"){
+        if (req.body.access_token != "concertina"){
             res.status(403).send();
         }
         exists = false;
         for (var i=0; i<people.length; i++){
-            if (people[i].username == req.get('username')){
+            if (people[i].username == req.body.username){
                 exists = true;
             }
         }
@@ -67,25 +99,73 @@ app.get('/people/:username', function (req,res) {
             res.status(400).send();
         } else {
             var user =  {
-            "forename" : req.get('forename'),
-            "surname" : req.get('surname'),
-            "username" : req.get('username'),
-            "password" : req.get('password'),
+            "forename" : req.body.forename,
+            "surname" : req.body.surname,
+            "username" : req.body.username,
          }
-         data = JSON.parse(data);
-         data = data + user;
+         fs.readFile("eople.json" ,function(err,data){
+            if (err){console.log(err)}
+            var people = JSON.parse(data);
+            people.push(user);
+            fs.writeFile("people.json" , JSON.stringify(people), function (err){
+                if (err){console.log(err)};
+            });
+        });
          res.end( JSON.stringify(data));
         }
     });
  });
- app.post("/api/Upload", function(req, res) {
-    upload(req, res, function(err) {
-        if (err) {
-            return res.end("Something went wrong!");
+//function that uploads images and stores their place and category in a json file
+ app.post("/api/Upload", function(req, res) { 
+    upload(req, res, function(err) {  
+        var newim = [];
+        var ex = exists(req.body.user)
+        console.log(ex);
+        if (exists(req.body.user)){
+            console.log("Truth");
+        for (i=0; i<req.files.length; i++){
+            if (req.files[i].mimetype != "image/jpeg" && req.files[i].mimetype != "image/png" ){
+                fs.unlink(req.files[i].path, (err) => {
+                    if (err) throw err;
+                    console.log(req.files[i].path+' was deleted, Wrong Type');
+                  });
+                return res.status(400).end("Incorect file type for: "+req.files[i].originalname+" Only JPEG or PNG files are accepted");
+            }
+            var newImage = {
+                "path" : req.files[i].path,
+                "category": req.body.category
+            };
+            newim.push(newImage);
         }
-        return res.end("File uploaded sucessfully!.");
+            fs.readFile("./Images/images.json" ,function(err,data){
+                if (err){console.log(err)}
+                var images = JSON.parse(data);
+                for (i=0; i<newim.length; i++){
+                    images.push(newim[i]);
+                }
+                fs.writeFile("./Images/images.json" , JSON.stringify(images), function (err){
+                    if (err){console.log(err)};
+                });
+            });
+        if (err instanceof multer.MulterError) {
+        } else if (err){
+            return res.status(400).end("Something went wrong!");
+        }
+        res.type('text');
+        
+        return res.status(201).end("Files uploaded sucessfully!");
+    }else{
+        for (i=0; i<req.files.length; i++){
+            console.log(req.files[0].path);
+        fs.unlink(req.files[i].path, (err) => {
+            if (err) throw err;
+            //console.log(req.files[i].path+' was deleted, Incorrect User');
+          });
+        }
+        return res.status(400).end("User: "+req.body.user+" was not found");
+    }
     });
-
+    
 });
 app.set('view engine', 'html');
 var options = {
@@ -94,12 +174,12 @@ var options = {
 };
 app.use(express.static('./', options));
 app.get('/' , function(req,res){
-    return res.sendFile(__dirname + '/index.html');
+    return res.status(200).sendFile(__dirname + '/index.html');
 });
 
 app.listen(port);
 console.log("The app is listening now on localhost:8080")
 module.exports = app;
 
-
-
+//possible categories:
+//technology, landscape, portrait, night photography
--- js/index.js
+++ js/index.js
@@ -1,19 +1,104 @@
-$("#frmUploader").submit(function(e) {
+function update(){
+    $(document).ready(function() {
+        var categories = ischecked();
+        if (categories.length!=0){
+        $.ajax({
+            url: '/images',
+            type: 'GET',
+            data: {"cat": categories},
+            datatype: 'json',
+            success(imagesST){
+                var images = JSON.parse(imagesST);
+                if (images.length){
+                $("#imshow").html('<div class ="container-fluid" id="im">');
+                for (i=0; i<images.length; i++){
+                    $("#imshow").append('<img src="'+images[i].path+'"class = "image"><p>'+images[i].category+'</p><div class="divider"></div>')
+                }
+                $("imshow").html('</div>');
+            }
+            },
 
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+            }   
 
-    var form = $(this);
-    var url = form.attr('action');
+                });
+            
+            } 
+        });
+}
+$(document).ready(function() {
 
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: form.serialize(), // serializes the form's elements.
-           success: function(data)
-           {
-               alert(data); // show response from the php script.
-           }
-         });
-    console.log("Works");
+$('#frmUploader').submit(function(e) {
+   $("#status").empty().text("File is uploading...");
+   $(this).ajaxSubmit({
 
-    e.preventDefault(); // avoid to execute the actual submit of the form.
+       error: function(xhr) {
+   status('Error: ' + xhr.status);
+       },
+
+       success: function(response) {
+   $("#status").empty().text(response);
+}
 });
+
+  //disables page return
+return false;
+}); 
+});
+
+$(document).ready(function() {
+        var categories = ischecked();
+    if (categories.length!=0){
+        $.ajax({
+            url: '/images',
+            type: 'GET',
+            data: {"cat": categories},
+            datatype: 'json',
+            success(imagesST){
+                var images = JSON.parse(imagesST);
+                if (images.length){
+                $("#imshow").html('<div class ="container-fluid" id="im">');
+                for (i=0; i<images.length; i++){
+                    $("#imshow").append('<img src="'+images[i].path+'"class = "image"><p>'+images[i].category+'</p><div class="divider"></div>')
+                }
+                $("imshow").html('</div>');
+            }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+            }   
+                });
+            
+            }  
+        });
+    
+
+function ischecked(){
+    cat = [];
+    if ($("#g").is(":checked")){
+        cat.push($("#g").val())
+    }
+    if ($("#n").is(":checked")){
+        cat.push($("#n").val())
+    }
+    if ($("#m").is(":checked")){
+        cat.push($("#m").val())
+    }
+    if ($("#p").is(":checked")){
+        cat.push($("#p").val())
+    }
+    if ($("#l").is(":checked")){
+        cat.push($("#l").val())
+    }
+    if ($("#a").is(":checked")){
+        cat.push($("#a").val())
+    }
+    if ($("#t").is(":checked")){
+        cat.push($("#t").val())
+    }
+    return cat;
+}
+
+
+
